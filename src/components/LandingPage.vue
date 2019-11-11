0a5ffc279f07b58ab2d2b90a7cf0ae78f606d0cd
Aimee <aimee@tcgplayer.com>
2019-11-11 10:48:34 -0500

    set variables and styles

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="landing-page">
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ .landing-page {
+  font-family: ShermanSans, "Trebuchet MS", Tahoma, sans-serif;
+  font-weight: normal;
+  font-style: normal;
+  color: $su-primary-blue;
+ }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   publicPath: '/syracuse-edu',
   outputDir: 'docs',
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "@/scss/main.scss";',
+      },
+    },
+  },
 };
